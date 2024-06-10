ace145cdc17063beb005195f3c1745c45553f410
William <william.sentosatio@hotmail.com>
2024-06-10 19:50:03 +0700

    temporary always allow cache

--- src/common/components/home/cache/utils.tsx
+++ src/common/components/home/cache/utils.tsx
@@ -1,5 +1,6 @@
 import { CACHE_PERMISSION_ID, CACHE_STATUS } from "../../constants"
 
 export const cacheIsAllowed = () => {
-    return localStorage.getItem(CACHE_PERMISSION_ID) === CACHE_STATUS.ALLOW
+    return true;
+    // return localStorage.getItem(CACHE_PERMISSION_ID) === CACHE_STATUS.ALLOW
 }
