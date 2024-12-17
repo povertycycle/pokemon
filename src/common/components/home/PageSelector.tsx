d906eac791ec2a002206d93023709028a6d35de3
William <william.sentosatio@hotmail.com>
2024-12-18 02:46:10 +0700

    temp

--- src/common/components/home/PageSelector.tsx
+++ src/common/components/home/PageSelector.tsx
@@ -24,11 +24,11 @@ const PageSelector: React.FC<PageSelectorProps> = ({ shouldAnimate, section, set
     return (
         <div className={`absolute flex flex-col z-[0] left-0 top-0 text-base-white font-vcr-mono`}>
             <div className={`${!!shouldAnimate ? `${styles.animateDown} transition-transform` : null} ${section !== PageState.Main ? "translate-y-[-100%]" : "translate-y-[0%]"} ${c}`} onClick={toDatabase}>
-                <Image fetchPriority="high" className={i} src={"img/mmgs.jpg"} width={640} height={360} alt="" />
+                <Image fetchPriority="high" className={i} src={"/pokemon/img/mmgs.jpg"} width={640} height={360} alt="" />
                 <div className={t}>DATABASE</div>
             </div>
             <div className={`${!!shouldAnimate ? `${styles.animateUp} transition-transform` : null} ${section !== PageState.Main ? "translate-y-[100%]" : "translate-y-[0%]"} ${c}`} onClick={toTCG}>
-                <Image fetchPriority="high" className={i} src={"img/mmcs.jpg"} width={640} height={360} alt="" />
+                <Image fetchPriority="high" className={i} src={"/pokemon/img/mmcs.jpg"} width={640} height={360} alt="" />
                 <div className={t}>TCG</div>
             </div>
         </div>
