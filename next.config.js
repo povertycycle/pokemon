36ca4027a448dc2c624ad2aaa4fa1897ef3dd8d2
William <william.sentosatio@hotmail.com>
2025-01-05 03:24:57 +0700

    test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // basePath: '/pokemon',
-    // assetPrefix: '/pokemon/',
+    assetPrefix: '/pokemon/',
     output: 'export',
     images: {
         minimumCacheTTL: 3000,
